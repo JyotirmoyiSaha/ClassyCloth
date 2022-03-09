760b41c231aeba660de7d6331d8d1fcd87a056d0
TahminaAkterUrmi <tahmina.urmi.0@gmail.com>
2022-03-09 11:20:15 +0600

    upto category crud

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\admin\CategoriesController;
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('admin.master');
-});
+
+
+
+// admin
+//  Route::group(['prefix'=>'admin','as'=>'admin.'], function(){
+
+    Route::get('/', function () {
+        return view('admin.master');
+     });
+    
+
+// categories
+Route::get('/category_list',[CategoriesController::class, 'list'])->name('category.list');
+Route::get('/category_create',[CategoriesController::class, 'create'])->name('category.create');
+Route::post('/category_store',[CategoriesController::class, 'store'])->name('category.store');
+Route::get('/category_edit/{category_id}',[CategoriesController::class, 'edit'])->name('category.edit');
+Route::post('/category_update/{category_id}',[CategoriesController::class, 'update'])->name('category.update');
+Route::get('/category_delete/{category_id}',[CategoriesController::class,'delete'])->name('category.delete');
+
+
+ 
+// });
